c9e1c78da4981aa413c1fb417dbda3f5369c5aec
Mikhail Vasilev <iwheelbuy@gmail.com>
2021-07-04 10:35:54 +0300

    +

--- step1.sh
+++ step1.sh
@@ -83,7 +83,7 @@ iptables -A INPUT -j DROP
 iptables -A FORWARD -j DROP
 netfilter-persistent save
 netfilter-persistent reload
-cd /home/admin
+cd
 openssl pkcs12 -export -in /etc/ipsec.d/certs/client.pem -inkey /etc/ipsec.d/private/client.pem -certfile /etc/ipsec.d/cacerts/root.pem -name "client" -out client.p12
 chmod 777 client.p12
 openssl verify -CAfile /etc/ipsec.d/cacerts/root.pem /etc/ipsec.d/certs/server.pem
