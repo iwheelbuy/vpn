06742964fc1d857fd17dcd075c5f84d7066832a3
Mikhail Vasilev <michael.vasiliev@exness.com>
2023-05-20 21:10:22 +0300

    +

--- step1.sh
+++ step1.sh
@@ -84,7 +84,7 @@ iptables -A FORWARD -j DROP
 netfilter-persistent save
 netfilter-persistent reload
 cd
-openssl pkcs12 -export -in /etc/ipsec.d/certs/client.pem -inkey /etc/ipsec.d/private/client.pem -certfile /etc/ipsec.d/cacerts/root.pem -name "client" -out $2.p12
+openssl pkcs12 -export -in /etc/ipsec.d/certs/client.pem -inkey /etc/ipsec.d/private/client.pem -certfile /etc/ipsec.d/cacerts/root.pem -name "client" -out $2.p12 -descert
 chmod 777 $2.p12
 openssl verify -CAfile /etc/ipsec.d/cacerts/root.pem /etc/ipsec.d/certs/server.pem
 openssl verify -CAfile /etc/ipsec.d/cacerts/root.pem /etc/ipsec.d/certs/client.pem
